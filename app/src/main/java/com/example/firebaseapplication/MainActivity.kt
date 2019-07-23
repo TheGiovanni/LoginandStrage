8f7a1e347cf96fd8ba97bdddcca941f05fabcd17
Giovanni <padde.cross@gmail.com>
2019-07-23 12:13:32 +0900

    signin Activity complete

--- app/src/main/java/com/example/firebaseapplication/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapplication/MainActivity.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -27,7 +28,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance()
         val user = mAuth.currentUser
+        val email = emailText.text.toString()
+        val password  = passwordText.text.toString()
         loginButton.setOnClickListener {
+//            if (email.isEmpty() || password.isEmpty()) {
+//                Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+//                return@setOnClickListener
+//            }
             mAuth.createUserWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString()).addOnCompleteListener(this){task ->
                 if (task.isSuccessful){
                     Toast.makeText(this, "ok",Toast.LENGTH_LONG).show()
@@ -67,9 +74,11 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
             selectedPhotoUri = data.data
 
-            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-            var bitmapDrawable = BitmapDrawable(bitmap)
-            selectPhoto_button.setBackgroundDrawable(bitmapDrawable)
+            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            selected_imageView.setImageBitmap(bitmap)
+            selectPhoto_button.alpha = 0f
+//            var bitmapDrawable = BitmapDrawable(bitmap)
+//            selectPhoto_button.setBackgroundDrawable(bitmapDrawable)
         }
     }
     private fun uploadImageToFireBaseStorage(){
@@ -85,7 +94,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun saveUserToFireBaseDatabase(uri: String){
+    private fun saveUserToFireBaseDatabase(imageUri: String){
+        val uid = FirebaseAuth.getInstance().uid ?:""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
 
+        val user = User(uid, imageUri)
+        ref.setValue(user).addOnSuccessListener {
+            Log.d("MainActivity", "saved")
+        }
     }
 }
---
+++ app/src/main/java/com/example/firebaseapplication/User.kt
@@ -0,0 +1,5 @@
+package com.example.firebaseapplication
+
+class User(val uid: String, val profileImageUrl: String){
+
+}
